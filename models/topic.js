cc7240c7d387cd7704f500009d06e81418e4b940
danofa <dmdanofar@gmail.com>
2015-06-21 00:07:27 +0200

    r1

---
+++ models/topic.js
@@ -0,0 +1,14 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var topicSchema = new mongoose.Schema({
+	title: { type: String, unique: true, required: '{PATH} is a required element!' },
+	description: { type: String, required: '{PATH} is a required element!' },
+	parent: { type: Schema.Types.ObjectId, default: null, ref: 'Topic'}
+});
+
+topicSchema.index({title : "text", description:"text"});
+
+mongoose.model('Topic', topicSchema);
